8506b1f8ad27ea95c135720f1ddad1e1a746811b
Oein <oein0219@gmail.com>
2023-06-18 14:23:11 +0900

    add meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import "@/styles/globals.css";
 import { ModalProvider } from "@team.poi/ui/dist/cjs/hooks/Modal";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ModalProvider>
+      <Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://poi.kr/" />
+        <meta property="og:description" content="Share everything you have." />
+        <meta property="og:site_name" content="poi.kr (포이)" />
+        <meta property="og:locale" content="en_US" />
+      </Head>
       <Component {...pageProps} />
     </ModalProvider>
   );
--- pages/custom.tsx
+++ pages/custom.tsx
@@ -1,9 +1,13 @@
 import Features from "@/components/features";
 import Home from "@/components/home";
+import Head from "next/head";
 
 export default function Page() {
   return (
     <>
+      <Head>
+        <meta property="og:title" content="poi.kr / Customized Path URL" />
+      </Head>
       <Home customed />
       <Features />
     </>
